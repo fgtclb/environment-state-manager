647e597d936b1a5e698730bdb01d105ac3b4c5af
Stefan Bürk <stefan@buerk.tech>
2026-06-21 10:10:12 +0200

    [TASK] Set dev version 1.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'FGTCLB: Environment State Manager',
     'description' => 'Environment builder and state manager for TYPO3 CMS.',
-    'version' => '1.0.0',
+    'version' => '1.0.1',
     'category' => 'misc',
     'state' => 'stable',
     'author' => 'FGTCLB',
